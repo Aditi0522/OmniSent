167f8c0613a5112349484cafc5d385e2ba66d930
Aditi Soni <aerilsoni05@gmail.com>
2026-05-22 18:37:19 +0530

    debugged logic errors

--- facial_module/face_detector.py
+++ facial_module/face_detector.py
@@ -47,19 +47,20 @@ class SCRFDFaceDetector:
 
             x1,y1,x2,y2 = bbox
             h,w = frame.shape[:2]
-            pad_x = int((x2-x1)*0.4)
-            pad_y = int((y2-y1)*0.4)
-            x1 = max(0,x1-pad_x)
-            y1 = max(0,y1-pad_y)
-            x2 = min(w,x2+pad_x)
-            y2 = min(h,y2+pad_y)
+            pad_x = int((x2-x1)*0.1)
+            pad_y = int((y2-y1)*0.1)
+            x1_new = max(0,x1-pad_x)
+            y1_new = max(0,y1-pad_y)
+            x2_new = min(w,x2+pad_x)
+            y2_new = min(h,y2+pad_y)
 
-            face_crop = frame[y1:y2, x1:x2]
+            face_crop = frame[y1_new:y2_new, x1_new:x2_new]
             if face_crop.size ==0:
                 self.logger.info("Cropped face size = 0")
                 return results
             results.append({
                 "bbox": [x1,y1,x2,y2],
+                "padded_box": [x1_new,y1_new,x2_new,y2_new],
                 "det_score": det_score,
                 "face_crop": face_crop
                 })
